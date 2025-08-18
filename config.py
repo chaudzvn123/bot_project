e19c239806e42910514cd1110a5455e71a572212
chaudzvn123 <jwjanwnuhhshsj@gmail.com>
2025-08-18 12:30:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
 
+# Thông tin cấu hình bot
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "MTQwNDc5MjgwMTUzNDY3NzAzMw.Ggl9bU.2lyyWTA274vAjNpKz9xPh1H-gggRiNYEnSZw38")
+ADMIN_UID = int(os.getenv("ADMIN_UID", 1401399647569838202))
+
+# Thông tin cấu hình GitHub
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "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")
+GITHUB_REPO = os.getenv("GITHUB_REPO", "chaudzvn123/bot_project")
+KEYS_FILEPATH = os.getenv("KEYS_FILEPATH", "keys.json")
